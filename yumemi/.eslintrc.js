a9bf1c9bc50c61331f1074bd613f6fd19fde8df9
Nke0628 <ooo_diva@yahoo.co.jp>
2022-07-15 23:26:02 +0900

    ESLintとprettierの最終設定

--- yumemi/.eslintrc.js
+++ yumemi/.eslintrc.js
@@ -1,19 +1,15 @@
+// eslint-disable-next-line no-undef
 module.exports = {
-    "env": {
-        "browser": true,
-        "es2021": true
-    },
-    "extends": [
-        "eslint:recommended",
-        "plugin:vue/vue3-essential"
-    ],
-    "parserOptions": {
-        "ecmaVersion": "latest",
-        "sourceType": "module"
-    },
-    "plugins": [
-        "vue"
-    ],
-    "rules": {
-    }
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: ['eslint:recommended', 'plugin:vue/vue3-essential', 'prettier'],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  plugins: ['vue'],
+  //個別ルール
+  rules: {},
 }
